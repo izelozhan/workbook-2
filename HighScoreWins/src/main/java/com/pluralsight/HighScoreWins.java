5eb523673b307ec7e6ac2f3021dbf78a9c074f3d
Izel Ozhan <izelozhan@gmail.com>
2025-04-15 13:43:49 -0500

    refactor exercise-3

--- HighScoreWins/src/main/java/com/pluralsight/HighScoreWins.java
+++ HighScoreWins/src/main/java/com/pluralsight/HighScoreWins.java
@@ -17,22 +17,18 @@ public class HighScoreWins {
 //        //hint ex
 //        String input = "Dallas|Ft. Worth|Austin";
 //        String[] cities = input.split("\\|");
-        String[] pipes = input.split("\\|");
+        String[] homesAndScores = input.split("\\|");
 //        String one = pipes[0];
 //        String two = pipes[1];
 //        System.out.println(one); //home:visitor
 //        System.out.println(two); //21:9
-
-        String[] homes = pipes[0].split(":");
-        String[] scores = pipes[1].split(":");
+        String[] homes = homesAndScores[0].split(":");
+        String[] scores = homesAndScores[1].split(":");
 
         String home1 = homes[0];
         String home2 = homes[1];
-        String score1 = scores[0];
-        String score2 = scores[1];
-
-        int intScore1 = Integer.parseInt(score1);
-        int intScore2 = Integer.parseInt(score2);
+        int intScore1 = Integer.parseInt(scores[0]);
+        int intScore2 = Integer.parseInt(scores[1]);
 
         if(intScore1 > intScore2){
             System.out.println("Winner: " + home1);
